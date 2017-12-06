4f0b9b4c3606685157f0a78242e0ac990442d347
PaDun <patrick.dunphy@wallawalla.edu>
2017-12-06 11:40:25 -0800

    Added tests for current user and non-existant user

---
+++ tests/aswwu/test_searchHandler.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+def test_searchTest(testing_server):
+    expected_data = {'results': [{'email':'ryan.rabello@wallawalla.edu',
+                    'full_name': 'Ryan Rabello',
+                    'photo':'profiles/1718/00958-2019687.jpg',
+                    'username':'ryan.rabello',
+                    'views':'9'}]}
+    url = "http://127.0.0.1:8888/search/1718/ryan.rabello"
+    resp = requests.get(url)
+    assert (resp.status_code == 200)
+    assert (json.loads(resp.text) == expected_data)
+
+def test_NotFound(testing_server):
+    expected_data = {'results': []}
+    url = "http://127.0.0.1:8888/search/1718/patty.dunp"
+    resp = requests.get(url)
+    assert (resp.status_code == 200)
+    assert (json.loads(resp.text) == expected_data)
+
+#def test_offYear(testing_server):
+
+#    url = "http://127.0.0.1:8888/search/1719/ryan.rabello"
+#    resp = requests.get(url)
+#    assert (resp.status_code == 200)
+#    assert (json.loads(resp.text) == expected_data)
+
+#def test_views(testing_server):
