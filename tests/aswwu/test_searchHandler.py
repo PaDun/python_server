f23e2990c380dc868b98811c4dd83eef6ed5cea9
Patrick Dunphy <patrick.dunphy@wallawalla.edu>
2017-12-07 10:24:32 -0800

    Update test_searchHandler.py

--- tests/aswwu/test_searchHandler.py
+++ tests/aswwu/test_searchHandler.py
@@ -19,11 +19,11 @@ def test_NotFound(testing_server):
     assert (resp.status_code == 200)
     assert (json.loads(resp.text) == expected_data)
 
-#def test_offYear(testing_server):
-
-#    url = "http://127.0.0.1:8888/search/1719/ryan.rabello"
-#    resp = requests.get(url)
-#    assert (resp.status_code == 200)
-#    assert (json.loads(resp.text) == expected_data)
+def test_offYear(testing_server):
+    expected_data = {'results': []}
+    url = "http://127.0.0.1:8888/search/1719/ryan.rabello"
+    resp = requests.get(url)
+    assert (resp.status_code == 200)
+    assert (json.loads(resp.text) == expected_data)
 
 #def test_views(testing_server): 
