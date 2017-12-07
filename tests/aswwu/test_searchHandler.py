a3491aea3cf13d80b2f7b186fcdb263fdf372e89
Patrick Dunphy <patrick.dunphy@wallawalla.edu>
2017-12-07 11:32:25 -0800

    Added second current search test for triangulation

--- tests/aswwu/test_searchHandler.py
+++ tests/aswwu/test_searchHandler.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-def test_searchTest(testing_server):
+def test_searchTest1(testing_server):
     expected_data = {'results': [{'username':'ryan.rabello',
                     'photo':'profiles/1718/00958-2019687.jpg',
                     'email':'ryan.rabello@wallawalla.edu',                                  
@@ -11,7 +11,18 @@ def test_searchTest(testing_server):
     resp = requests.get(url)
     assert (resp.status_code == 200)
     assert (json.loads(resp.text) == expected_data)
-
+    
+def test_searchTest2(testing_server):
+    expected_data = {'results': [{'username':'john.doe',
+                    'photo':'profiles/1718/00958-2019687.jpg',
+                    'email':'john.doe@wallawalla.edu',                                  
+                    'full_name': 'John Doe',
+                    'views':'43'}]}
+    url = "http://127.0.0.1:8888/search/1718/ryan.rabello"
+    resp = requests.get(url)
+    assert (resp.status_code == 200)
+    assert (json.loads(resp.text) == expected_data)
+ 
 def test_NotFound(testing_server):
     expected_data = {'results': []}
     url = "http://127.0.0.1:8888/search/1718/patty.dunp"
