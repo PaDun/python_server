c2112def0687d9b817ac51e12f47758fc685c6e2
PaDun <patrick.dunphy@wallawalla.edu>
2017-12-07 15:01:26 -0800

    adding tests, getting it

--- tests/aswwu/test_searchHandler.py
+++ tests/aswwu/test_searchHandler.py
@@ -1,40 +1,177 @@
 import requests
 import json
+from tests.utils import profile, archived_profile, gen_profiles
 
-def test_searchTest1(testing_server):
-    expected_data = {'results': [{'username':'ryan.rabello',
-                    'photo':'profiles/1718/00958-2019687.jpg',
-                    'email':'ryan.rabello@wallawalla.edu',                                  
-                    'full_name': 'Ryan Rabello',
-                    'views':'9'}]}
-    url = "http://127.0.0.1:8888/search/1718/ryan.rabello"
-    resp = requests.get(url)
+
+def test_search_specific_current(testing_server, peopledb_conn):
+    expected_data = {'results': [{
+                    'email':'None',
+                    'full_name':'None',
+                    'photo':'profiles/1617/00958-2019687.jpg',
+                    'username':'test.profile1',
+                    'views':'1'
+                    }]}
+    with profile(peopledb_conn, list(gen_profiles(number = 3))):
+        url = "http://127.0.0.1:8888/search/1718/test.profile1"
+        resp = requests.get(url)
     assert (resp.status_code == 200)
     assert (json.loads(resp.text) == expected_data)
-    
-def test_searchTest2(testing_server):
-    expected_data = {'results': [{'username':'john.doe',
-                    'photo':'profiles/1718/00958-2019687.jpg',
-                    'email':'',                                  
-                    'full_name': 'John Doe',
-                    'views':'6'}]}
-    url = "http://127.0.0.1:8888/search/1718/john.doe"
-    resp = requests.get(url)
+
+
+def test_search_specific_archive(testing_server, archivesdb_conn):
+    expected_data = {'results': [{
+                    'email':'None',
+                    'full_name':'None',
+                    'photo':'profiles/1617/00958-2019687.jpg',
+                    'username':'test.profile1',
+                    'views':'None'
+                    }]}
+    with archived_profile(archivesdb_conn, list(gen_profiles(number = 3))):
+        url = "http://127.0.0.1:8888/search/1617/test.profile1"
+        resp = requests.get(url)
     assert (resp.status_code == 200)
     assert (json.loads(resp.text) == expected_data)
- 
-def test_NotFound(testing_server):
+
+
+def test_search_single_letter_current(testing_server, peopledb_conn):
+    expected_data = {
+        "results": [{
+            'email':'None',
+            'full_name':'None',
+            'photo':'profiles/1617/00958-2019687.jpg',
+            'username':'test.profile0',
+            'views':'0'
+        },{
+            'email':'None',
+            'full_name':'None',
+            'photo':'profiles/1617/00958-2019687.jpg',
+            'username':'test.profile1',
+            'views':'1'
+        },{
+            'email':'None',
+            'full_name':'None',
+            'photo':'profiles/1617/00958-2019687.jpg',
+            'username':'test.profile2',
+            'views':'0'
+        }]
+    }
+    with profile(peopledb_conn, list(gen_profiles(number = 3))):
+        url = 'http://127.0.0.1:8888/search/1718/t'
+        resp = requests.get(url)
+    assert (resp.status_code == 200)
+    assert (json.loads(resp.text) == expected_data)
+
+
+def test_search_all_current(testing_server, peopledb_conn):
+    expected_data = {
+        "results": [{
+            'email':'None',
+            'full_name':'None',
+            'photo':'profiles/1617/00958-2019687.jpg',
+            'username':'test.profile0',
+            'views':'0'
+        },{
+            'email':'None',
+            'full_name':'None',
+            'photo':'profiles/1617/00958-2019687.jpg',
+            'username':'test.profile1',
+            'views':'1'
+        },{
+            'email':'None',
+            'full_name':'None',
+            'photo':'profiles/1617/00958-2019687.jpg',
+            'username':'test.profile2',
+            'views':'0'
+        }]
+    }
+
+    with profile(peopledb_conn, list(gen_profiles(number = 3))):
+        url = 'http://127.0.0.1:8888/search/all'
+        resp = requests.get(url)
+    assert (resp.status_code == 200)
+    assert (json.loads(resp.text) == expected_data)
+
+
+def test_search_single_letter_archive(testing_server, archivesdb_conn):
+    expected_data = {
+        "results": [{
+            'email':'None',
+            'full_name':'None',
+            'photo':'profiles/1617/00958-2019687.jpg',
+            'username':'test.profile0',
+            'views':'None'
+        },{
+            'email':'None',
+            'full_name':'None',
+            'photo':'profiles/1617/00958-2019687.jpg',
+            'username':'test.profile1',
+            'views':'None'
+        },{
+            'email':'None',
+            'full_name':'None',
+            'photo':'profiles/1617/00958-2019687.jpg',
+            'username':'test.profile2',
+            'views':'None'
+        }]
+    }
+
+    with archived_profile(archivesdb_conn, list(gen_profiles(number = 3))):
+        url = 'http://127.0.0.1:8888/search/1617/t'
+        resp = requests.get(url)
+    assert (resp.status_code == 200)
+    assert (json.loads(resp.text) == expected_data)
+
+
+def test_NotFound(testing_server, peopledb_conn):
     expected_data = {'results': []}
-    url = "http://127.0.0.1:8888/search/1718/patty.dunp"
-    resp = requests.get(url)
+    with profile(peopledb_conn, list(gen_profiles(number = 3))):
+        url = 'http://127.0.0.1:8888/search/1718/d'
+        resp = requests.get(url)
     assert (resp.status_code == 200)
     assert (json.loads(resp.text) == expected_data)
 
-def test_offYear(testing_server):
-    expected_data = {'results': []}
-    url = "http://127.0.0.1:8888/search/1719/ryan.rabello"
-    resp = requests.get(url)
+
+def test_search_subgroup_current(testing_server, peopledb_conn):
+    expected_data = {
+        'results': [{
+            'email':'None',
+            'full_name':'None',
+            'photo':'profiles/1617/00958-2019687.jpg',
+            'username':'test.profile1',
+            'views':'1'
+        },{
+            'email':'None',
+            'full_name':'None',
+            'photo':'profiles/1617/00958-2019687.jpg',
+            'username':'test.profile10',
+            'views':'0'
+        }]}
+    with profile(peopledb_conn, list(gen_profiles(number = 11))):
+        url = 'http://127.0.0.1:8888/search/1718/e1'
+        resp = requests.get(url)
     assert (resp.status_code == 200)
     assert (json.loads(resp.text) == expected_data)
 
-#def test_views(testing_server): 
+
+def test_search_subgroup_archive(testing_server, archivesdb_conn):
+    expected_data = {
+        'results': [{
+            'email':'None',
+            'full_name':'None',
+            'photo':'profiles/1617/00958-2019687.jpg',
+            'username':'test.profile2',
+            'views':'None'
+        },{
+            'email':'None',
+            'full_name':'None',
+            'photo':'profiles/1617/00958-2019687.jpg',
+            'username':'test.profile20',
+            'views':'None'
+        }]}
+    with archived_profile(archivesdb_conn, list(gen_profiles(number = 21))):
+        url = 'http://127.0.0.1:8888/search/1617/e2'
+        resp = requests.get(url)
+    assert (resp.status_code == 200)
+    assert (json.loads(resp.text) == expected_data)
+
+#def test_views(testing_server):
