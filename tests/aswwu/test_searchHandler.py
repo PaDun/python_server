27a2656cbff8a57c472a2dad2bf813c9ed53d815
Patrick Dunphy <patrick.dunphy@wallawalla.edu>
2017-12-07 11:03:28 -0800

    Update test_searchHandler.py

--- tests/aswwu/test_searchHandler.py
+++ tests/aswwu/test_searchHandler.py
@@ -2,10 +2,10 @@ import requests
 import json
 
 def test_searchTest(testing_server):
-    expected_data = {'results': [{'email':'ryan.rabello@wallawalla.edu',
-                    'full_name': 'Ryan Rabello',
+    expected_data = {'results': [{'username':'ryan.rabello',
                     'photo':'profiles/1718/00958-2019687.jpg',
-                    'username':'ryan.rabello',
+                    'email':'ryan.rabello@wallawalla.edu',                                  
+                    'full_name': 'Ryan Rabello',
                     'views':'9'}]}
     url = "http://127.0.0.1:8888/search/1718/ryan.rabello"
     resp = requests.get(url)
