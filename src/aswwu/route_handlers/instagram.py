9f7ba0379380bd13d4638bda73230128321c8ced
PaDun <patrick.dunphy@wallawalla.edu>
2017-12-08 14:28:38 -0800

    Instagram fix fo' real though

--- src/aswwu/route_handlers/instagram.py
+++ src/aswwu/route_handlers/instagram.py
@@ -12,30 +12,33 @@ class FeedHandler(BaseHandler):
     def get(self):
         name = self.get_argument('name', '')
         if name == "atlas":
-            with open("/var/www/atlas/access.token", 'r') as f:
-                token = f.read()
-                f.close()
-                token = token.rstrip('\n')
-                # TODO: Make this asynchronous and move access.token to aswwu/databases git repo
-                http_client = HTTPClient()
-                try:
-                    response = http_client.fetch("https://api.instagram.com/v1/users/self/media/recent/?access_token="
-                                                 + token)
-                    self.write(response.body)
-                except Exception as e:
-                    self.write("{error: '" + str(e) + "'}")
-                http_client.close()
+            self.get_atlas()
         elif name == "issuu":
-            get_issuu()
+            self.get_issuu()
         else:
             self.write("Something went wrong.")
+
+    def get_atlas(self):
+        with open("/var/www/atlas/access.token", 'r') as f:
+            token = f.read()
+            f.close()
+            token = token.rstrip('\n')
+            # TODO: Make this asynchronous and move access.token to aswwu/databases git repo
+            http_client = HTTPClient()
+            try:
+                response = http_client.fetch("https://api.instagram.com/v1/users/self/media/recent/?access_token="
+                                             + token)
+                self.write(response.body)
+            except Exception as e:
+                self.write("{error: '" + str(e) + "'}")
+            http_client.close()
 
     def get_issuu(self):
         http_client = HTTPClient()
         try:
             response = http_client.fetch("http://search.issuu.com/api/2_0/"
-                "document?username=aswwucollegian&pageSize=1&responseParams="
-                "title,description&sortBy=epoch")
+                    "document?username=aswwucollegian&pageSize=1"
+                    "&responseParams=title,description&sortBy=epoch")
             self.write(response.body)
         except Exception as e:
             self.write("{error: '" + str(e) + "'}")
